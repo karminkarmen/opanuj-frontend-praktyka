c18372491622c4b5e2e7002740bbc557b2a4c7a9
Kamila Skonieczna-Opielka <95kamila.skonieczna@gmail.com>
2024-02-25 16:10:06 +0100

    cleanup: validation input type

--- examples/module1/lesson1/validate-it/validation/validate.ts
+++ examples/module1/lesson1/validate-it/validation/validate.ts
@@ -3,11 +3,10 @@ const isEven = (inputValue: number) => inputValue % 2 === 0;
 const getIfIsGreaterThan = (min: number) => (inputValue: number) =>  inputValue > min;
 const getIfIsLowerThan = (max: number) => (inputValue: number) => inputValue < max;
 
-export const validate = (inputValue: number | null): boolean => {
+export const validate = (inputValue: number): boolean => {
   if (!inputValue) {
     return false;
   }
-
   const isGreaterThan = getIfIsGreaterThan(0);
   const isLowerThan = getIfIsLowerThan(100);
 
